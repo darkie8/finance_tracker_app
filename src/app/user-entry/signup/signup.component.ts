6a00d5df29a8372b03b2c3cec1bd860803c93b5f
darkie9 <sdarkei9@gmail.com>
2018-12-12 22:30:03 +0530

    last

--- src/app/user-entry/signup/signup.component.ts
+++ src/app/user-entry/signup/signup.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers: [MessageService]
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+  email: string;
+  password: string;
+  constructor(private router: Router,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
   }
+  /**
+   * signUp
+   */
+  public signUp() {
+    if (!this.userName) {
+      this.messageService.add({
+        key: 'un',
+        severity: 'warn',
+        summary: 'userName missing',
+        detail: 'input userName'
+      });
 
+    } else if (!this.email) {
+      this.messageService.add({
+        key: 'em',
+        severity: 'warn',
+        summary: 'email missing',
+        detail: 'input email'
+      });
+    }  else if (!this.password) {
+      this.messageService.add({
+        key: 'pass',
+        severity: 'warn',
+        summary: 'password missing',
+        detail: 'input password'
+      });
+    } else {
+      this.messageService.add({
+      key: 'signUp',
+      severity: 'info',
+      summary: 'SignUp Done !!',
+      detail: 'Soon, you will be directed to Login.'
+    });
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 1000);
+  }
+  }
 }
